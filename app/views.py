0c0ac36342189807d31170de9ee281d4708d83f4
DineshMaddamsetty <dinesh.maddamsetty@gmail.com>
2023-06-16 13:43:37 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+def aishu(request):
+    return HttpResponse('<center>Aishu is Best Friend of Dinesh</center>')
+    
+
+
+
+# Create your views here.
